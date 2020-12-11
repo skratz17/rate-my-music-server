36eb8be9fe438d5963fab257f24df96cd5f4225e
Jacob Eckert <jweckert17@gmail.com>
2020-12-11 11:59:23 -0600

    routing for ArtistViewSet

--- server/urls.py
+++ server/urls.py
@@ -1,8 +1,15 @@
 """server URL Configuration"""
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
 from rmmapi.views import login_user, register_user
+from rmmapi.views import ArtistViewSet
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'artists', ArtistViewSet, 'artist')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user)
 ]
