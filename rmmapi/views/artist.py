c714cce29873c097019197db889db6cbfe1986b4
Jacob Eckert <jweckert17@gmail.com>
2020-12-11 11:50:04 -0600

    implement artist create method

--- rmmapi/views/artist.py
+++ rmmapi/views/artist.py
@@ -1,4 +1,41 @@
 """Artist ViewSet and Serializers"""
+from django.core.exceptions import ValidationError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework import serializers
+from rest_framework import status, serializers
+from rmmapi.models import Artist, Rater
+
+class ArtistViewSet(ViewSet):
+    def create(self, request):
+        """POST a new artist"""
+        missing_keys = self._get_missing_keys()
+        if len(missing_keys) > 0:
+            return Response(
+                { 'message': f"Request body is missing the following required properties: {', '.join(missing_keys)}"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        rater = Rater.objects.get(user=request.auth.user)
+
+        artist = Artist(
+            name=request.data['name'],
+            description=request.data['description'],
+            founded_year=request.data['year'],
+            creator=rater
+        )
+
+        try:
+            artist.save()
+        except ValidationError as ex:
+            return Response({ "message": ex.message }, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({}, status=status.HTTP_201_CREATED)
+
+    def _get_missing_keys(self):
+        """Given the request.data for a POST/PUT request, return a list containing the
+        string values of all required keys that were not found in the request body"""
+        REQUIRED_KEYS = [
+            'name', 'description', 'founded_year'
+        ]
+
+        return [ key for key in REQUIRED_KEYS if not key in self.request.data ]
