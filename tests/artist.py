b95cc767f8bea354d6a5f3658902a7831f62f85f
Jacob Eckert <jweckert17@gmail.com>
2020-12-11 11:55:39 -0600

    write test for successful artist create

---
+++ tests/artist.py
@@ -0,0 +1,37 @@
+import json
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+class ArtistTests(APITestCase):
+    def setUp(self):
+        """Create an account and set up auth header for future HTTP requests"""
+        data = {
+            'username': 'jweckert17',
+            'email': 'jweckert17@gmail.com',
+            'password': 'test',
+            'first_name': 'Jacob',
+            'last_name': 'Eckert',
+            'bio': 'I am just a cool boi.'
+        }
+
+        response = self.client.post('/register', data, format='json')
+        json_response = json.loads(response.content)
+        self.token = json_response['token']
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+
+    def test_create_artist(self):
+        """Create a valid artist"""
+        data = {
+            'name': 'The Magnetic Fields',
+            'founded_year': 1990,
+            'description': 'An amazing band.'
+        }
+
+        response = self.client.post('/artists', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        created_artist = json.loads(response.content)
+        self.assertEqual(created_artist['id'], 1)
+        self.assertEqual(created_artist['name'], 'The Magnetic Fields')
+        self.assertEqual(created_artist['founded_year'], 1996)
+        self.assertEqual(created_artist['description'], 'An amazing band.')
