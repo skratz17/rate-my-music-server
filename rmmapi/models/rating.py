2a1b27b36e6546313f0751920afa458d6a8126ce
Jacob Eckert <jweckert17@gmail.com>
2020-12-23 13:20:06 -0600

    allow blank review field in Rating model

--- rmmapi/models/rating.py
+++ rmmapi/models/rating.py
@@ -4,7 +4,7 @@ from .rater import get_deleted_rater_instance
 
 class Rating(models.Model):
     rating = models.IntegerField(validators=[ MinValueValidator(1), MaxValueValidator(5) ])
-    review = models.CharField(max_length=2000)
+    review = models.CharField(max_length=2000, blank=True)
     song = models.ForeignKey("rmmapi.Song", on_delete=models.CASCADE, related_name="ratings")
     rater = models.ForeignKey("rmmapi.Rater", on_delete=models.SET(get_deleted_rater_instance))
     created_at = models.DateTimeField(auto_now=False, auto_now_add=False)
