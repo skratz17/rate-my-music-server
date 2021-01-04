94ffa2b25349dc88429b71aa8ee479f7f450af86
Jacob Eckert <jweckert17@gmail.com>
2021-01-04 21:20:42 +0000

    added http://ratemymusic.jacobweckert.com as a valid cors origin

--- server/settings.py
+++ server/settings.py
@@ -54,15 +54,16 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-CORS_ORIGIN_WHITELIST = (
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
-)
+    'http://127.0.0.1:3000',
+    'http://ratemymusic.jacobweckert.com'
+]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
